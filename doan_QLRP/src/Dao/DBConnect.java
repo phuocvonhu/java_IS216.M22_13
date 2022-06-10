be27dadaaaa4e83c860faabf9a5b98f8d2dd7476
dao minh tuan <20520842@gm.uit.edu.vn>
2022-06-11 00:07:02 +0700

    update Model and Dao from Tuan

---
+++ doan_QLRP/src/Dao/DBConnect.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dao;
+
+/**
+ *
+ * @author minht
+ */
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+        
+
+public class DBConnect {
+    public static Connection getConnectionSqlServer(String username, String password) {
+        Connection cons = null;
+        try {
+            String dbURL = "jdbc:sqlserver://localhost\\MSSQLSERVER01:1433;databaseName=QLHV";
+            cons = DriverManager.getConnection(dbURL, username, password);
+            if (cons != null) {
+                System.out.println("Connected");
+                DatabaseMetaData dm = (DatabaseMetaData) cons.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cons;
+    }
+    public static Connection getConnectionMySql(String username, String password){
+        Connection cons = null;
+        try{
+            String url = "jdbc:mysql://localhost:3306/QLHV";
+            cons = DriverManager.getConnection(url, username, password);
+            if (cons != null) {
+                System.out.println("Connected");
+                DatabaseMetaData dm = (DatabaseMetaData) cons.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+            }
+        }
+        catch(Exception ex){
+           System.out.println(ex.getMessage());
+        }
+        return cons;
+    }
+}
